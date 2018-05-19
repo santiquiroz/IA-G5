bc0a79695b43d686447c458abc2d8cf75a9d457c
kolive360 <kolive360@hotmail.com>
2018-05-18 21:51:02 -0500

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -9,6 +9,6 @@ import graphlab
 
 #cargamos la base de datos
 url = 'https://github.com/santiquiroz/IA-G5/blob/PruebasRedNeuronal/file.csv'
-sf = graphlab.SFrame.read_csv(url)
-sf
+training_data = graphlab.SFrame.read_csv(url)
+training_data, validation_data = data.random_split(0.8) #80% Aprendizaje, 20% Validacion
 
